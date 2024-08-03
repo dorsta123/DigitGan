5ee30ffc0a96340367dec3af0946b9da37e47e6c
dorsta123 <sahil.bh.m@gmail.com>
2024-08-03 18:46:10 +0530

    added the function to convert tensor to images

--- Generator.py
+++ Generator.py
@@ -15,3 +15,19 @@ dataloader = DataLoader(
     MNIST('.', download=True, transform=transforms.ToTensor()),
     batch_size=batch_size,
     shuffle=True)
+
+#function for converting the fetched tensor to images
+def tensor_to_images(image_tensor, num_images=30, size=(1, 28, 28)):
+    image_unflat = image_tensor.detach().cpu().view(-1, *size)
+    image_grid = make_grid(image_unflat[:num_images], nrow=6)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+#code block to check whether the data is loaded successfully or not
+"""
+sample_image=next(iter(dataloader))
+sample_image[0].shape
+torch.Size([128,1,28,28])
+sample_image[-1][0:30]
+tensor_to_images(sample_image[0])
+"""
