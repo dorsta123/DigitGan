1bcef738e2b33cbf1aa036511bd8ab6234254002
dorsta123 <sahil.bh.m@gmail.com>
2024-08-03 18:38:31 +0530

    ignored some files

--- Generator.py
+++ Generator.py
@@ -8,3 +8,10 @@ from torchvision.utils import make_grid
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 torch.manual_seed(123)
+
+# fetching the MNIST dataset
+batch_size = 128
+dataloader = DataLoader(
+    MNIST('.', download=True, transform=transforms.ToTensor()),
+    batch_size=batch_size,
+    shuffle=True)
