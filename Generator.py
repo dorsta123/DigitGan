81e39693dcc9f44290aef20f92c7793d7a6c7f2d
dorsta123 <sahil.bh.m@gmail.com>
2024-08-03 16:46:50 +0530

    installing and importing all the necessary libraries

--- Generator.py
+++ Generator.py
@@ -1,1 +1,10 @@
-
+#importing the necessary libraries
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST 
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(123)
